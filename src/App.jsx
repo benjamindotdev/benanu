3f1fe67871d1174562ae745074ac05083e4c74b4
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-07-23 15:56:33 +0200

    making usermap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,4 @@
 import "./index.css";
-import {
-  APIProvider,
-  Map,
-  Marker,
-  Pin,
-  useMap,
-} from "@vis.gl/react-google-maps";
-import { useEffect, useState } from "react";
-import UserInputForm from "./components/UserInput";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
@@ -15,11 +6,6 @@ import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import DashboardPage from "./pages/DashboardPage";
 import ResultPage from "./pages/ResultPage";
-
-const ironhack = {
-  lat: 52.53308,
-  lng: 13.45321,
-};
 
 //link for data "https://ourworldindata.org/travel-carbon-footprint";
 
@@ -29,23 +15,6 @@ const socials = {
 };
 
 const App = () => {
-  const map = useMap();
-
-  const [destination, setDestination] = useState("");
-
-  useEffect(() => {
-    if (!map) return;
-    map.setOptions({
-      styles: [{ featureType: "poi", stylers: [{ visibility: "off" }] }],
-    });
-  }, [map]);
-
-  useEffect(() => {
-    console.log(destination);
-  }, [destination]);
-
-  //add routes, route, links from react-router-dom
-
   return (
     <div className="mx-auto w-[600px] md:w-[700px] lg:w-[1000px] xl:[1200px]">
       <Navbar socials={socials} />
@@ -57,30 +26,6 @@ const App = () => {
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/result" element={<ResultPage />} />
         </Routes>
-
-        <APIProvider
-          apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-          className="flex flex-col justify-center"
-        >
-          <UserInputForm
-            destination={destination}
-            setDestination={setDestination}
-          />
-          <Map
-            style={{ width: "500px", height: "500px" }}
-            defaultCenter={{ ...ironhack }}
-            defaultZoom={15}
-            gestureHandling={"greedy"}
-          >
-            <Marker position={{ ...ironhack }}>
-              <Pin
-                background={"#FBBC04"}
-                glyphColor={"#000"}
-                borderColor={"#000"}
-              />
-            </Marker>
-          </Map>
-        </APIProvider>
       </main>
     </div>
   );
