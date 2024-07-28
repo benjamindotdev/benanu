f2d33217378f88e608eda7116541e91599f503f2
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-07-28 22:08:08 +0200

    fixing destination search

---
+++ src/components/DestinationInput.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import PageSubHeader from "./PageSubHeader";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useThemeContext } from "../context/ThemeContext";
+
+export default function UserInputForm() {
+  //const { postData } = useTripContext();
+  const [destination, setDestination] = useState("");
+  const [responses, setResponses] = useState([]);
+  const { theme } = useThemeContext();
+
+  const handleChange = (e) => {
+    //postData({ destination });
+    setDestination(e.target.value);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      destination.length > 2 &&
+        axios
+          .get(
+            `https://graphhopper.com/api/1/geocode?q=${destination}&locale=en&key=${
+              import.meta.env.VITE_GRAPHHOPPER_API_KEY
+            }`
+          )
+          .then((response) => {
+            setResponses(response.data.hits);
+          })
+          .catch((error) => {
+            console.log(
+              error.response,
+              import.meta.env.VITE_GRAPHHOPPER_API_KEY
+            );
+          })
+          .finally(() => {
+            console.log(responses);
+          });
+    }, 300);
+  }, [responses, destination]);
+
+  useEffect(() => {
+    destination === "" && setResponses([]);
+  }, [destination]);
+
+  return (
+    <div
+      className={`flex flex-col gap-2 p-6 rounded-xl card shadow-xl w-full ${
+        theme === "light" ? "bg-light2" : "bg-dark2"
+      }`}
+    >
+      <div className="flex justify-between">
+        <PageSubHeader>Enter your destination</PageSubHeader>
+        <a
+          href="https://www.graphhopper.com"
+          target="_blank"
+          rel="noreferrer"
+          className=" text-accent text-lg text-right"
+        >
+          Powered by Graphhopper
+        </a>
+      </div>
+      <div className="flex flex-row gap-2 items-center ">
+        <input
+          type="text"
+          value={destination}
+          onChange={(e) => handleChange(e)}
+          placeholder="Enter your destination"
+          className={`input w-full shadow-xl rounded-md border-secondary focus:border-accent focus:ring focus:ring-primary focus:ring-opacity-50 ${
+            theme === "light" ? "bg-light text-dark" : "bg-dark text-light"
+          }`}
+        />
+        {/* <button
+          className={`btn border-0 shadow-xl hover:bg-secondary ${
+            theme === "light" ? "bg-light text-dark" : "bg-dark text-light"
+          }`}
+          onClick={(e) => handleClick(e)}
+        >
+          Search
+        </button> */}
+      </div>
+      <ul className="flex flex-col gap-2">
+        {responses &&
+          destination !== "" &&
+          responses.map((response, index) => (
+            <Link
+              to={`/result?lat=${response.point.lat}&lng=${response.point.lng}&destination=${response.name}`}
+              key={response.osm_id + index}
+              className={`flex flex-row gap-2 p-2 font-bold opacity-50 hover:bg-accent hover:opacity-100 hover:text-dark ${
+                theme === "light" ? "bg-light2 " : "bg-dark2"
+              }`}
+            >
+              <p>
+                {response.name} ({response.osm_value}), {response.country}
+              </p>
+            </Link>
+          ))}
+      </ul>
+    </div>
+  );
+}
--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import { useThemeContext } from "../context/ThemeContext";
+
 const profiles = {
   foot: { icon: "🚶", label: "foot" },
   bike: { icon: "🚲", label: "bike" },
@@ -9,10 +11,16 @@ const Result = ({ distance, emissions, time, profile }) => {
   const hours = Math.floor(time / 60);
   const minutes = Math.ceil(time % 60);
 
+  const { theme } = useThemeContext();
+
   return (
-    <div className="flex flex-col w-[400px] gap-2 card border-2 border-light p-6 text-light">
+    <div
+      className={`flex flex-col mx-auto w-[500px] gap-2 card shadow-xl border-light p-6 ${
+        theme === "light" ? "text-dark bg-light2" : "text-light bg-dark2"
+      }`}
+    >
       <>
-        <h2 className="text-2xl text-primary self-center">
+        <h2 className="text-2xl font-bold text-primary self-center">
           {profiles[profile].icon}{" "}
           {profiles[profile].label[0].toUpperCase() +
             profiles[profile].label.slice(1)}
