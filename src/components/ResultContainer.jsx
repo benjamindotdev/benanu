4d3c5eb0d39fa269fc617aeaab8470b4cc811a36
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-07-30 15:14:35 +0200

    removing imports

--- src/components/ResultContainer.jsx
+++ src/components/ResultContainer.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import ResultCard from "./ResultCard";
 import emissions from "../data/emissions.json";
