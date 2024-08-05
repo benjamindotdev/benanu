acbd6761e3f93be8df8a07e0e770a206f7ffbca6
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-08-05 22:03:18 +0200

    adjusting structure for context

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,30 @@ import { BrowserRouter } from "react-router-dom";
 import { TripProvider } from "./context/TripProvider";
 import { InfoProvider } from "./context/InfoContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { AuthProviderWrapper } from "./context/AuthContext.jsx";
 
 const root = createRoot(document.getElementById("root"));
 
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <ThemeProvider>
+//         <AuthProviderWrapper>
+//           <App />
+//         </AuthProviderWrapper>
+//       </ThemeProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <TripProvider>
         <InfoProvider>
           <ThemeProvider>
-            <App />
+            <AuthProviderWrapper>
+              <App />
+            </AuthProviderWrapper>
           </ThemeProvider>
         </InfoProvider>
       </TripProvider>
